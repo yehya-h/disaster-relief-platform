a77b255a049b70d084d9f3055d987e9780d1dd89
HananElKadi <hanan.hk.kadi@gmail.com>
2025-07-27 14:43:25 +0300

    update the maindrawer

--- frontend/drp/src/routes/MainDrawer.jsx
+++ frontend/drp/src/routes/MainDrawer.jsx
@@ -1,29 +1,28 @@
-import React from 'react';
-// import { createDrawerNavigator } from '@react-navigation/drawer';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
 // import MainStack from './MainStack.jsx';
 import Profile from '../screens/Profile.jsx';
 import Posts from '../screens/Posts.jsx';
 import TabNavigator from './TabNavigator.jsx';
 
-// const Drawer = createDrawerNavigator();
-const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+// const Stack = createNativeStackNavigator();
 
 export default function MainDrawer() {
   return (
-    <Stack.Navigator
+    <Drawer.Navigator
       screenOptions={{
         headerShown: true, // Let drawer show the top bar with toggle
       }}
     >
-      <Stack.Screen
+      <Drawer.Screen
         name="Homee"
         component={TabNavigator}
         options={{ headerShown: false }}
       />
-      <Stack.Screen name="Posts" component={Posts} />
+      <Drawer.Screen name="Posts" component={Posts} />
 
-      <Stack.Screen name="Profile" component={Profile} />
-    </Stack.Navigator>
+      <Drawer.Screen name="Profile" component={Profile} />
+    </Drawer.Navigator>
   );
 }
